d07dd25261565fdd42ae2be4f28293e3b038a211
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-08-08 04:01:02 +0000

    Finalizado

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,20 +1,32 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/trafficlight.css";
 
 const TrafficLight = () => {
     const [color, setColor] = useState("red");
-    const [redOn, setRedOn] = useState (false);
-    const [greenOn, setGreenOn] = useState (false);
+    const [redOn, setRedOn] = useState(false);
+    const [greenOn, setGreenOn] = useState(false);
     const [yellowOn, setYellowOn] = useState(false);
 
     return <>
-    <div className="stick justify-content-center"></div>
+        <div className="stick justify-content-center"></div>
         <div className="container rounded">
             <div className="row d-flex justify-content-center">
                 <div className="col d-flex justify-content-center flex-column align-items-center">
-                    <div className={!redOn ? "red" : "red on"} onClick={(e) => {setRedOn(!redOn)}}>Red</div>
-                    <div className={!yellowOn ? "yellow" : "yellow on"} onClick={(e) => {setYellowOn(!yellowOn)}}>Yellow</div>
-                    <div className={!greenOn ? "green" : "green on"} onClick={(e) => {setGreenOn(!greenOn)}}>Green</div>
+                    <div className={redOn == false ? "red" : "red on"} onClick={(e) => {
+                        setRedOn(!redOn)
+                        setYellowOn(false)
+                        setGreenOn(false)
+                    }}>Red</div>
+                    <div className={yellowOn == false ? "yellow" : "yellow on"} onClick={(e) => {
+                        setRedOn(false)
+                        setYellowOn(!yellowOn)
+                        setGreenOn(false)
+                    }}>Yellow</div>
+                    <div className={greenOn == false ? "green" : "green on"} onClick={(e) => {
+                        setRedOn(false)
+                        setYellowOn(false)
+                        setGreenOn(!greenOn)
+                    }}>Green</div>
                 </div>
             </div>
 
